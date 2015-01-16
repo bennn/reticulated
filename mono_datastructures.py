295b7e940c49e6d1472060888de04f38f5dc12aa
Ben Greenman <types@ccs.neu.edu>
2015-01-16 03:22:16 -0500

    [monotonic-not-working] fixed import error

--- mono_datastructures.py
+++ mono_datastructures.py
@@ -1,4 +1,4 @@
-from monotonic import retic_cast
+import monotonic
 from relations import info_join
 import rtypes
 
@@ -14,26 +14,26 @@ class MonoList(list):
         self.__monotonic__ = type
     
     def __setitem__(self, k, v):
-        v = retic_cast(v, rtypes.Dyn, self.__monotonic__, self.__error__, line=self.__line__)
+        v = monotonic.retic_cast(v, rtypes.Dyn, self.__monotonic__, self.__error__, line=self.__line__)
         super().__setitem__(k, v)
         
     def __setitem_attype__(self, k, v, t):
-        v = retic_cast(v, t, self.__monotonic__, self.__error__, line=self.__line__)
+        v = monotonic.retic_cast(v, t, self.__monotonic__, self.__error__, line=self.__line__)
         super().__setitem__(k, v)
 
     def __getitem__(self, k):
         v = super().__getitem__(k)
-        return retic_cast(v, self.__monotonic__, rtypes.Dyn, self.__error__, line=self.__line__)
+        return monotonic.retic_cast(v, self.__monotonic__, rtypes.Dyn, self.__error__, line=self.__line__)
         
     def __getitem_attype__(self, k, t):
         v = super().__getitem__(k)
-        return retic_cast(v, self.__monotonic__, t, self.__error__, line=self.__line__)
+        return monotonic.retic_cast(v, self.__monotonic__, t, self.__error__, line=self.__line__)
 
     def __monotonic_cast__(self, ty, error, line):
         newmono = info_join(ty, self.__monotonic__)
         if newmono != self.__monotonic__:
             for i in range(self):
-                v = retic_cast(self.__fastgetitem__(i), self.__monotonic__, newmono, error, line=self.line)
+                v = monotonic.retic_cast(self.__fastgetitem__(i), self.__monotonic__, newmono, error, line=self.line)
                 self.__fastsetitem__(i, v)
             self.__monotonic__ = newmono
             self.__line__ = line
